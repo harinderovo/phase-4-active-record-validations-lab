5eff56446f8955c1332282fe21bf0985fde3fb25
Harinder Singh <harmiglany@yahoo.com>
2023-01-31 16:49:21 -0800

    commit tree 72d3bae77951e3d82884b0de89192fc2bb099da1

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    # validates :name, presence: true, uniqueness: true {case_sensitive: false}
+    validates :name, uniqueness: true
+    validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :title, format: { with: /Won't Believe|Secret|Top \d|Guess/}
+    
+    # validates :title,
+    # def must_not_have_true_facts exclusion: { without: (True Facts) }
+
+    # validate :true_facts 
+    # def capitalize_category_name
+    #     self.category = self.category.capitalize
+    # end
+
+    # def true_facts
+    #     if [/Wont Believe/i, /Secret/i, /Top \d/i, /Guess/i].none? {|pattern| pattern.match(self.title)}
+    #     end
+    # end
+
+
 end
